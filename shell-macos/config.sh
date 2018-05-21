2419226a7734af80ef45993be3723f5079093b68
Damir Imaev <dimaev@gsngames.com>
2018-05-21 13:36:22 +0100

    Fixed iterm2 integration.

--- shell-macos/config.sh
+++ shell-macos/config.sh
@@ -1,1 +1,1 @@
-test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
