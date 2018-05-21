cfc1dc2367396742af014b7e45932808103ba963
Damir Imaev <dimaev@gsngames.com>
2018-05-21 17:57:17 +0100

    Added configration file for VS Code.

--- shell-macos/config.sh
+++ shell-macos/config.sh
@@ -1,1 +1,10 @@
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+VS_CODE_DEFAULT_SETTINGS_PATH="$HOME/Library/Application Support/Code/User/settings.json"
+VS_CODE_CUSTOM_SETTINGS_PATH="$HOME/.vscode.settings"
+if [ -e $VS_CODE_CUSTOM_SETTINGS_PATH ]; then
+  if [ -f $VS_CODE_DEFAULT_SETTINGS_PATH ]; then
+    rm $VS_CODE_DEFAULT_SETTINGS_PATH
+  fi
+  ln -s $VS_CODE_CUSTOM_SETTINGS_PATH $VS_CODE_DEFAULT_SETTINGS_PATH
+fi
