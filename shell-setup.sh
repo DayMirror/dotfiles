43d879d8d80d900646772570be5df1e7555f1f95
Damir Imaev <dimaev@gsngames.com>
2018-05-21 13:35:20 +0100

    Some fixes for shell setup and zshrc.

--- shell-setup.sh
+++ shell-setup.sh
@@ -4,6 +4,8 @@ shell_prefix='shell'
 shell_scripts=('variables' 'aliases' 'functions' 'config')
 shell_includes=('common')
 
+echo "shell directory is $shell_directory"
+
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
   platform='linux'
@@ -16,15 +18,17 @@ if [ ! -z "$platform" ]; then
 fi
 
 # shell_includes+=('gsn')
+shell_includes+=('local')
 
 for shell_include in  "${shell_includes[@]}"
 do
-  shell_folder="$shell_directory/$shell_prefix-$shell_include"
+  shell_folder="$shell_directory/.$shell_prefix-$shell_include"
   if [ -d $shell_folder ]; then
     for shell_script in "${shell_scripts[@]}"
     do
       shell_file="$shell_folder/$shell_script.sh"
-      if [ -f $shell_file ]; then
+      if [ -e $shell_file ]; then
+        echo "included file $shell_file"
         source $shell_file
       fi
     done
--- zshrc
+++ zshrc
@@ -1,5 +1,5 @@
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
 export ZSH=~/.oh-my-zsh
@@ -71,7 +71,15 @@ plugins=(
   autojump
 )
 
-source $ZSH/oh-my-zsh.sh
+oh_my_zsh=$ZSH/oh-my-zsh.sh
+if [ -e $oh_my_zsh ]; then
+  source $oh_my_zsh
+fi
+
+shell_setup=~/.shell-setup.sh
+if [ -e $shell_setup ]; then
+  source $shell_setup
+fi
 
 # User configuration
 
