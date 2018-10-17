5527a69f5854622b39957c362b55a9736437b5c8
Damir Imaev <dimaev@gsngames.com>
2018-10-17 11:59:32 +0100

    Added new aliases to quckly start new unity instance.

--- shell-macos/aliases.sh
+++ shell-macos/aliases.sh
@@ -1,1 +1,4 @@
 alias screendocker="screen $DOCKER_VM"
+alias unitystart="open -na $UNITY_PATH/Unity.app"
+alias unity5start="open -na $UNITY_PATH_5/Unity.app"
+alias unity2018start="open -na $UNITY_PATH_2018/Unity.app"
--- shell-macos/variables.sh
+++ shell-macos/variables.sh
@@ -3,5 +3,7 @@ ANDROID_SDK=$ANDROID_PATH/sdk
 ANDROID_NDK=$ANDROID_PATH/ndk
 ANDROID_HOME=$ANDROID_SDK
 UNITY_PATH=/Applications/Unity
+UNITY_PATH_5=/Applications/Unity.5.3.8p3
+UNITY_PATH_2018=/Applications/Unity.2018.2
 PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$ANDROID_NDK:$UNITY_PATH/Unity.app/Contents/MacOS
 DOCKER_VM=$HOME/Library/Containers/com.docker.docker/Data/vms/0/tty
