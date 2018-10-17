c1ccd4068a5cde66fe7074d0cdbc32a3fb1d2dac
Damir Imaev <dimaev@gsngames.com>
2018-10-17 16:21:23 +0100

    Removed modifying PATH variable.

--- shell-macos/variables.sh
+++ shell-macos/variables.sh
@@ -5,5 +5,4 @@ ANDROID_HOME=$ANDROID_SDK
 UNITY_PATH=/Applications/Unity
 UNITY_PATH_5=/Applications/Unity.5.3.8p3
 UNITY_PATH_2018=/Applications/Unity.2018.2
-PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_SDK/platform-tools:$ANDROID_NDK:$UNITY_PATH/Unity.app/Contents/MacOS
 DOCKER_VM=$HOME/Library/Containers/com.docker.docker/Data/vms/0/tty
