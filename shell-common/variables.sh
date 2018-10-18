94450b20b77c495fd462714a88148e7f9bde5a3f
Damir Imaev <dimaev@gsngames.com>
2018-10-18 16:35:25 +0100

    Exporting environment variables.

--- shell-common/variables.sh
+++ shell-common/variables.sh
@@ -1,5 +1,5 @@
-DEV_PATH=$HOME/dev
-REPOS_PATH=$DEV_PATH/repos
-BUILDS_PATH=$DEV_PATH/builds
-ZSHEBANG="#!/usr/bin/env zsh"
-SHEBANG="#!/usr/bin/env bash"
+export DEV_PATH=$HOME/dev
+export REPOS_PATH=$DEV_PATH/repos
+export BUILDS_PATH=$DEV_PATH/builds
+export ZSHEBANG="#!/usr/bin/env zsh"
+export SHEBANG="#!/usr/bin/env bash"
--- shell-macos/variables.sh
+++ shell-macos/variables.sh
@@ -1,8 +1,8 @@
-ANDROID_PATH=~/Library/Android
-ANDROID_SDK=$ANDROID_PATH/sdk
-ANDROID_NDK=$ANDROID_PATH/ndk
-ANDROID_HOME=$ANDROID_SDK
-UNITY_PATH=/Applications/Unity
-UNITY_PATH_5=/Applications/Unity.5.3.8p3
-UNITY_PATH_2018=/Applications/Unity.2018.2
-DOCKER_VM=$HOME/Library/Containers/com.docker.docker/Data/vms/0/tty
+export ANDROID_PATH=~/Library/Android
+export ANDROID_SDK=$ANDROID_PATH/sdk
+export ANDROID_NDK=$ANDROID_PATH/ndk
+export ANDROID_HOME=$ANDROID_SDK
+export UNITY_PATH=/Applications/Unity
+export UNITY_PATH_5=/Applications/Unity.5.3.8p3
+export UNITY_PATH_2018=/Applications/Unity.2018.2
+export DOCKER_VM=$HOME/Library/Containers/com.docker.docker/Data/vms/0/tty
