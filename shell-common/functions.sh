d373a8256ae7a774e8eecaf6cff6e11ed35e4168
Damir Imaev <dimaev@gsngames.com>
2019-06-09 23:50:26 +0100

    Added renamextension command.

--- shell-common/functions.sh
+++ shell-common/functions.sh
@@ -19,3 +19,15 @@ getabspath()
 {
 	echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
+
+renamextension()
+{
+	local source_extension="$1"
+	local target_extension="$2"
+	local dir="$3"
+	if [ -z $source_extension ] || [ -z $target_extension ] || [ -z $dir ]; then
+		echo "Invalid command invokation\nUsage: 'renameextension original_extension new_extension directory'"
+		return 1
+	fi
+	find "$dir" -depth -type f -name "*.$source_extension" -exec echo 'Renaming ' {} \; -execdir rename "s/\.$source_extension$/\.$target_extension/" {} \;
+}
