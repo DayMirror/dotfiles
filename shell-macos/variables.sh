c303907c4c7f3823e9374ec62738517e795f8bdc
Damir Imaev <dimaev@gsngames.com>
2019-12-18 15:38:07 +0000

    Added anaconda init logic in zshrc.

--- shell-macos/variables.sh
+++ shell-macos/variables.sh
@@ -3,4 +3,4 @@ export ANDROID_SDK=$ANDROID_PATH/sdk
 export ANDROID_NDK=$ANDROID_PATH/ndk
 export ANDROID_HOME=$ANDROID_SDK
 export DOCKER_VM=$HOME/Library/Containers/com.docker.docker/Data/vms/0/tty
-export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$ANDROID_NDK:$ANDROID_NDK/build:$ANDROID_SDK/tools/bin:/usr/local/anaconda3/bin
+export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$ANDROID_NDK:$ANDROID_NDK/build:$ANDROID_SDK/tools/bin
