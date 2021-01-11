ab8367aed7efcbaf2d1a312f521c190a0b748546
Damir Imaev <dimaev@gsngames.com>
2021-01-11 21:22:09 +0000

    Sourcing kubernetes completion files.

--- shell-macos/config.sh
+++ shell-macos/config.sh
@@ -8,3 +8,12 @@ if [ -e $VS_CODE_CUSTOM_SETTINGS_PATH ]; then
   fi
   ln -s $VS_CODE_CUSTOM_SETTINGS_PATH $VS_CODE_DEFAULT_SETTINGS_PATH
 fi
+
+gcloud_completions_file="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+if [ -f $gcloud_completions_file ]; then
+	source $gcloud_completions_file
+fi
+gcloud_path_file="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+if [ -f $gcloud_path_file ]; then
+	source $gcloud_path_file
+fi
