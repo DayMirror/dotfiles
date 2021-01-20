bd61ebe58ba1328123a9bc2e92aff2a9da8d4861
Damir Imaev <dimaev@gsngames.com>
2021-01-20 14:33:42 +0000

    Added GOPATH.

--- shell-common/variables.sh
+++ shell-common/variables.sh
@@ -3,3 +3,5 @@ export REPOS_PATH=$DEV_PATH/repos
 export BUILDS_PATH=$DEV_PATH/builds
 export ZSHEBANG="#!/usr/bin/env zsh"
 export SHEBANG="#!/usr/bin/env bash"
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
