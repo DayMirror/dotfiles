8a6b3f5e9d0f92862b301ff67cf1425db340116b
Damir Dev <damir.imaev.dev@gmail.com>
2024-09-26 15:03:41 +0100

    Removed dependency on missing file.

--- shell-macos/config.sh
+++ shell-macos/config.sh
@@ -1,14 +1,5 @@
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-VS_CODE_DEFAULT_SETTINGS_PATH="$HOME/Library/Application Support/Code/User/settings.json"
-VS_CODE_CUSTOM_SETTINGS_PATH="$HOME/.vscode.settings"
-if [ -e $VS_CODE_CUSTOM_SETTINGS_PATH ]; then
-  if [ -f $VS_CODE_DEFAULT_SETTINGS_PATH ]; then
-    rm $VS_CODE_DEFAULT_SETTINGS_PATH
-  fi
-  ln -s $VS_CODE_CUSTOM_SETTINGS_PATH $VS_CODE_DEFAULT_SETTINGS_PATH
-fi
-
 gcloud_completions_file="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 if [ -f $gcloud_completions_file ]; then
 	source $gcloud_completions_file
